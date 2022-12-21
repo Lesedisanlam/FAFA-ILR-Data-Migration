a6185018c198ba240b66b3554015bc21a6af2a12
Lesedisanlam <92445799+Lesedisanlam@users.noreply.github.com>
2022-12-21 10:13:51 +0200

    commit

--- Data Migration/Base/Base.cs
+++ Data Migration/Base/Base.cs
@@ -1,16 +1,262 @@
-namespace Data_Migration.NewFolder
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System;
+using OpenQA.Selenium.Firefox;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Data_Migration
+
 {
-    public class Tests
+
+    [TestFixture]
+
+    public class TestBase
+
     {
+
+        private ChromeOptions _chromeOptions;
+
+        public IWebDriver _driver;
+
+        private string _userName;
+
+        private string _password;
+
+        public string _screenShotFolder;
+
+        public string _connString;
+
+
+
+
         [SetUp]
-        public void Setup()
+
+        public void StartBrowser()
         {
+
+            _driver = new ChromeDriver("C:/Code/bin");
+
+            _connString = "Provider= Microsoft.ACE.OLEDB.12.0;" + "Data Source=C:/Users/G992107/Documents/Github/FE/FAFA-ILR-Data-Migration/Data Migration/Data Migration/TestData.xlsx" + ";Extended Properties='Excel 8.0;HDR=Yes'";
+
+            _screenShotFolder = $@"C:\Users\G992107\Documents\GitHub\ILR_TestSuite\Failed_ScreenShots​{ScreenShotDailyFolderName()}​\";
+
+            new DirectoryInfo(_screenShotFolder).Create();
+
+
         }
 
-        [Test]
-        public void Test1()
+        private static string ScreenShotDailyFolderName()
+
         {
-            Assert.Pass();
+
+            return DateTime.Now.ToString("yyyyMMdd").Replace("AM", string.Empty).Replace("PM", string.Empty);
+
         }
+
+        public void TakeScreenshot(IWebDriver driver, string filePath, string fileName)
+
+        {
+
+            if (!Directory.Exists(filePath))
+
+                new DirectoryInfo(filePath).Create();
+
+
+            ITakesScreenshot ssdriver = driver as ITakesScreenshot;
+
+            Screenshot screenshot = ssdriver.GetScreenshot();
+
+            fileName = $"{fileName}{ScreenShotTime()}.png";
+
+            screenshot.SaveAsFile($"{filePath}{fileName}", ScreenshotImageFormat.Png);
+
+            // byte[] byteArray = ((ITakesScreenshot)driver).GetScreenshot().AsByteArray;
+
+            //Bitmap ss  = new Bitmap(new System.IO.MemoryStream(byteArray));
+
+            // screenshot.SaveAsFile(String.Format(fileName, ImageFormat.Png));
+
+        }
+
+        private static string ScreenShotTime()
+
+        {
+
+            return DateTime.Now.TimeOfDay.ToString().Replace(":", "_").Replace(".", "_");
+
+        }
+
+
+
+        public IWebDriver SiteConnection()
+
+        {
+
+
+            _driver.Url = "http://ilr-tst.safrican.co.za/web/wspd_cgi.sh/WService=wsb_ilrtst/run.w";
+
+            _userName = "e569123";//TODO add your user name and password
+
+            _password = "June@062022";
+
+            _driver.Manage().Window.Maximize();
+
+            System.Threading.Thread.Sleep(2000);
+
+            _driver.Manage().Window.Maximize();
+
+            System.Threading.Thread.Sleep(2000);
+
+            IWebElement loginTextBox = _driver.FindElement(By.XPath("/html/body/center/center/form[2]/div/table/tbody/tr/td/span/table/tbody/tr[2]/td/div/center/div/table/tbody/tr[4]/td[2]/span/table/tbody/tr[2]/td[2]/input"));
+            IWebElement passwordTextBox = _driver.FindElement(By.CssSelector("#CntContentsDiv2 > table > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=password]"));
+            IWebElement loginBtn = _driver.FindElement(By.CssSelector("#GBLbl-1 > span > a"));
+            loginTextBox.SendKeys(_userName);
+            System.Threading.Thread.Sleep(4000);
+            passwordTextBox.SendKeys(_password);
+            System.Threading.Thread.Sleep(4000);
+            loginBtn.Click();
+            System.Threading.Thread.Sleep(2000);
+            return _driver;
+        }
+        public string GetPolicyNoFromExcell(string sheet, string function)
+        {
+
+
+            string conRef = "";
+            using (OleDbConnection conn = new OleDbConnection(_connString))
+            {
+                try
+                {
+
+                    // Open connection
+                    conn.Open();
+                    string cmdQuery = "SELECT * FROM [" + sheet + "$]";
+
+                    OleDbCommand cmd = new OleDbCommand(cmdQuery, conn);
+
+                    // Create new OleDbDataAdapter
+                    OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                    oleda.SelectCommand = cmd;
+
+                    // Create a DataSet which will hold the data extracted from the worksheet.
+                    DataSet ds = new DataSet();
+
+                    // Fill the DataSet from the data extracted from the worksheet.
+                    oleda.Fill(ds, "Policies");
+
+
+
+                    foreach (var row in ds.Tables[0].DefaultView)
+                    {
+
+                        var func = ((System.Data.DataRowView)row).Row.ItemArray[1].ToString();
+
+                        if (func == function)
+                        {
+                            conRef = ((System.Data.DataRowView)row).Row.ItemArray[0].ToString();
+                        }
+                    }
+
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+
+
+                    conn.Dispose();
+                }
+            }
+
+            return conRef;
+
+        }
+        
+
+        public void writeResultsToExcell(string results, string sheet, string function)
+        {
+            using (OleDbConnection conn = new OleDbConnection(_connString))
+            {
+                try
+                {
+
+                    // Open connection
+                    conn.Open();
+                    OleDbCommand cmd = conn.CreateCommand();
+
+                    var testDate = DateTime.Now.ToString();
+
+                    //Test_Date
+                    cmd.CommandText = $"UPDATE [{sheet}$] SET Test_Date = '{testDate}' WHERE Function = '{function}';";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = $"UPDATE [{sheet}$] SET Test_Results  = '{results}' WHERE Function = '{function}';";
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+            }
+
+
+
+
+        }
+
+
+        public void DisconnectBrowser()
+
+        {
+
+            if (_driver != null)
+
+                _driver.Quit();
+
+        }
+
+
+
+        public string GetSeleniumFormatTag(string inputControlName)
+
+        {
+
+            var result = $"//*[@id=\"{inputControlName}\"]";
+
+            return result;
+
+        }
+
+
+
+        public void Delay(int delaySeconds)
+
+        {
+
+            Thread.Sleep(delaySeconds * 1000);
+
+        }
+
+
+
     }
+
 }
--- Data Migration/Data Migration/DataVerification.cs
+++ Data Migration/Data Migration/DataVerification.cs
@@ -1,12 +1,319 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using Data_Migration;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Linq;
+    using System.Text;
+    using Actions = OpenQA.Selenium.Interactions.Actions;
 
-namespace Data_Migration.Data_Migration
+    namespace Data_Migration.Data_Migration
+
 {
-    internal class Class1
+
+        [TestFixture]
+
+        public class DataVerification : TestBase
     {
+
+            //Policy-Servicing
+
+            private string sheet;
+
+
+
+            [SetUp]
+
+            public void startBrowser()
+
+            {
+
+                _driver = TestBase.SiteConnection();
+                sheet = "Policy-Verification";
+
+            }
+
+
+
+            [Test, Order(1)]
+
+            public void PolicyServicingTestSuite()
+
+            {
+
+                using (OleDbConnection conn = new OleDbConnection(_connString))
+                {
+                    try
+                    {
+
+                        // Open connection
+                        conn.Open();
+                        string cmdQuery = "SELECT * FROM [" + sheet + "$]";
+
+                        OleDbCommand cmd = new OleDbCommand(cmdQuery, conn);
+
+                        // Create new OleDbDataAdapter
+                        OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                        oleda.SelectCommand = cmd;
+
+                        // Create a DataSet which will hold the data extracted from the worksheet.
+                        DataSet ds = new DataSet();
+
+                        // Fill the DataSet from the data extracted from the worksheet.
+                        oleda.Fill(ds, "Policies");
+
+
+
+                        foreach (var row in ds.Tables[0].DefaultView)
+                        {
+                            var contractRef = ((System.Data.DataRowView)row).Row.ItemArray[0].ToString(); ;
+                            var func = ((System.Data.DataRowView)row).Row.ItemArray[1].ToString();
+
+                            if (contractRef != "" && func != "")
+                            {
+                                Delay(8);
+                                clickOnMainMenu();
+                                try
+                                {
+                                    switch (func)
+                                    {
+                                        case "Dataverification":
+                                        Dataverification(contractRef);
+                                            break;
+
+                                        default:
+                                            break;
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    // TakeScreenshot(_driver, $@"{_screenShotFolder}\Failed_Scenarios\", func);
+                                    var errMsg = ex.Message;
+                                    StringBuilder error = new StringBuilder();
+                                    var charsToRemove = new char[] { '@', ',', '.', ';', '"', '{', '}' };
+                                    var stringsToRemove = new String[] { "'" };
+
+
+                                    foreach (char c in errMsg)
+                                    {
+                                        var isInvalidChar = charsToRemove.Contains(c);
+                                        var isInvalidString = stringsToRemove.Contains(c.ToString());
+                                        if (!isInvalidChar && !isInvalidString)
+                                        {
+                                            error.Append(c);
+                                        }
+                                    }
+
+                                    cmd = conn.CreateCommand();
+
+                                    var testDate = DateTime.Now.ToString();
+                                    var errorMsg = error.ToString();
+                                    if (errorMsg.Length > 250)
+                                    {
+                                        errorMsg = errorMsg.Substring(0, 250);
+                                    }
+
+                                    //Test_Date
+                                    cmd.CommandText = $"UPDATE [{sheet}$] SET Test_Date = '{testDate}' WHERE Function = '{func}';";
+                                    cmd.ExecuteNonQuery();
+                                    cmd.CommandText = $"UPDATE [{sheet}$] SET Test_Results  = 'Failed' WHERE Function = '{func}';";
+                                    cmd.ExecuteNonQuery();
+                                    cmd.CommandText = $"UPDATE [{sheet}$] SET Comments  = '{errorMsg}' WHERE Function = '{func}';";
+                                    cmd.ExecuteNonQuery();
+                                }
+
+                            }
+
+                        }
+
+
+                    }
+                    finally
+                    {
+                        conn.Close();
+                        conn.Dispose();
+                    }
+                }
+
+
+
+
+            }
+            private void clickOnMainMenu()
+            {
+                try
+                {
+                    //find the contract search option
+                    var search = _driver.FindElement(By.XPath("//*[@id='t0_764']/a"));
+                }
+                catch
+                {
+                    //clickOnMainMenu
+                    _driver.FindElement(By.Name("CBWeb")).Click();
+                }
+            }
+
+            public void Dataverification(string contractRef)
+
+
+
+        {
+
+
+
+            String test_url_1 = "http://ilr-int.safrican.co.za/web/wspd_cgi.sh/WService=wsb_ilrint/run.w?";
+            String test_url_1_title = "MIP - Sanlam ARL - Warpspeed Lookup Window";
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+
+
+
+
+            string results = "";
+
+
+
+            string date = DateTime.Today.ToString("g");
+
+
+
+
+            policySearch(contractRef);
+
+
+
+            //Contract Status validation
+            Delay(2);
+            SetproductName("ReInstate");
+            var Cancelled = _driver.FindElement(By.XPath("//*[@id='CntContentsDiv8']/table/tbody/tr[2]/td[2]/u/font")).Text;
+
+
+
+
+
+            IWebElement policyOptionElement3 = _driver.FindElement(By.XPath("//*[@id='m0i0o1']"));
+
+
+
+
+            //Creating object of an Actions class
+            Actions action2 = new Actions(_driver);
+
+
+
+
+
+            //Performing the mouse hover action on the target element.
+            action2.MoveToElement(policyOptionElement3).Perform();
+            Delay(2);
+
+
+
+            //Click on Reinstate
+            _driver.FindElement(By.XPath("//*[@id='m0t0']/tbody/tr[8]/td/div/div[3]/a/img")).Click();
+            Delay(2);
+
+            SelectElement oSelect2 = new SelectElement(_driver.FindElement(By.Name("frmReason")));
+            oSelect2.SelectByValue("ReinstateReason2");
+            Delay(2);
+
+
+            //Click submit
+            _driver.FindElement(By.Name("btnctcrereinstatecsu5")).Click();
+            Delay(4);
+
+
+
+
+            //Click submit
+            _driver.FindElement(By.Name("btnctcrereinstatecsu2")).Click();
+            Delay(5);
+
+
+
+
+
+            //Contract Status validation
+
+
+
+            var StatusInForce = _driver.FindElement(By.XPath("//*[@id='CntContentsDiv8']/table/tbody/tr[2]/td[2]/u/font")).Text;
+
+
+
+
+
+            Assert.IsTrue(StatusInForce.Equals("In-Force", StringComparison.CurrentCultureIgnoreCase));
+
+
+
+            Delay(3);
+
+
+
+            if (StatusInForce == "In-Force")
+            {
+                results = "Passed";
+            }
+            else
+            {
+                results = "Failed";
+            }
+
+
+
+            base.writeResultsToExcell(results, sheet, "ReInstate");
+        }
+
+        [Category("Policy Search")]
+        public void policySearch(string contractRef)
+        {
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+
+
+            //Click on contract search 
+            _driver.FindElement(By.Name("alf-ICF8_00000222")).Click();
+            Delay(2);
+
+
+
+
+            //Type in contract ref 
+
+            _driver.FindElement(By.Name("frmContractReference")).SendKeys(contractRef);
+
+
+
+            Delay(4);
+
+            //Click on Search Icon 
+            _driver.FindElement(By.Name("btncbcts0")).Click();
+
+            Delay(5);
+            _driver.FindElement(By.Name("hl_960735872.488")).Click();
+
+            Delay(5);
+
+        }
+
+
+        [TearDown]
+
+        public void closeBrowser()
+
+        {
+
+            base.DisconnectBrowser();
+
+        }
+
+
+
     }
+
 }
+
